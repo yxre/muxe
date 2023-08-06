4571a4123dff584d38bcc3153dbfd4e25a121d18
Dovile <lenurea@gmail.com>
2023-08-06 18:43:02 +0300

    Cleanup svg props warnings in react

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,8 @@ export const Navbar = () => {
         <a href="#" className="flex items-center">
           <img src="/img/logo.png" className="h-8 mr-3" alt="Muise Logo" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            <span className="text-2xl font-light">m</span>UX<span className="text-2xl font-light">e</span>
+            <span className="text-2xl font-light">m</span>UX
+            <span className="text-2xl font-light">e</span>
           </span>
         </a>
         <button
@@ -27,9 +28,9 @@ export const Navbar = () => {
           >
             <path
               stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M1 1h15M1 7h15M1 13h15"
             />
           </svg>
